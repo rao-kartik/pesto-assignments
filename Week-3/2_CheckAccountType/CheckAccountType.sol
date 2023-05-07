eccbdb3541b838818b0cbf277f5815af9390de71
Kartik Yadav <kartikyadav@Kartiks-MacBook-Pro.local>
2023-05-08 00:22:28 +0530

    improvement(CheckAccountTypw.sol): updated readme file

--- Week-3/2_CheckAccountType/CheckAccountType.sol
+++ Week-3/2_CheckAccountType/CheckAccountType.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+/**
+ * @title Contract to check if the address is of an EOA or a contract
+ * @author Kartik Yadav
+ * @notice This can be used to identify the type of account for a given address
+ */
+
 // import "@openzeppelin/contracts/utils/Address.sol";
 
 // contract CheckAccount {
@@ -15,16 +21,17 @@ pragma solidity ^0.8.17;
 // }
 
 contract CheckAccount {
-
-    function checkAccountType (address _addr) external view returns(string memory) {
+    function checkAccountType(
+        address _addr
+    ) external view returns (string memory) {
         uint codeSize;
         assembly {
             codeSize := extcodesize(_addr)
         }
 
-        if(codeSize > 0){
+        if (codeSize > 0) {
             return "Contract Account";
         }
         return "EOA";
-    }
+    }`
 }
--- Week-3/2_CheckAccountType/Readme.md
+++ Week-3/2_CheckAccountType/Readme.md
@@ -10,3 +10,14 @@ Solved using two ways:
 1. Using open zepplin library
 2. using assembly extcodesize
     If its a contract the size will be greater than 0 and hence is a contract address else EOA
+
+# Difference between Externally Owned Accounts (EOA) and Contract Accounts
+
+1. EOAs are owned by the users while Smart contract are independent of addresses/accounts.
+2. EOAs are controlled by the private key of the user while Smart Contracts are controlled by the compiler using the contract code.
+
+# What is extcodesize?
+
+Extcodesize is an opcode that returns the size of an account's code. It can be used to identify whether the account is an EOA or a contract account.
+
+If the extcodesize is  > 0 then its a contract account.

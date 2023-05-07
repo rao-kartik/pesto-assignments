5bfbcde10399f184a4a784a56277e770cf98ce1c
Kartik Yadav <kartikyadav@Kartiks-MacBook-Pro.local>
2023-05-08 00:04:45 +0530

    improvement(Calculator.sol): added description about contract

--- Week-3/1_Calculator/Calculator.sol
+++ Week-3/1_Calculator/Calculator.sol
@@ -1,26 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract Calculator {
+/**
+ * @title Calculator app
+ * @author Kartik Yadav
+ * @notice You can use this contract for basic mathematical operations
+ */
 
-    function addition (uint256 num1, uint256 num2) external pure returns(uint256) {
+contract Calculator {
+    
+    function addition(
+        uint256 num1,
+        uint256 num2
+    ) external pure returns (uint256) {
         return num1 + num2;
     }
 
-    function subtraction (int256 num1, int256 num2) external pure returns(int256) {
+    function subtraction(
+        int256 num1,
+        int256 num2
+    ) external pure returns (int256) {
         return num1 - num2;
     }
 
-    function multiplication (uint256 num1, uint256 num2) external pure returns(uint256) {
+    function multiplication(
+        uint256 num1,
+        uint256 num2
+    ) external pure returns (uint256) {
         return num1 * num2;
     }
 
-    function division (uint256 num1, uint256 num2) external pure returns(uint256) {
+    function division(
+        uint256 num1,
+        uint256 num2
+    ) external pure returns (uint256) {
         require(num2 != 0, "Can't divide by 0");
         return num1 / num2;
     }
 
-    function modulo (uint256 num1, uint256 num2) external pure returns(uint256) {
+    function modulo(
+        uint256 num1,
+        uint256 num2
+    ) external pure returns (uint256) {
         require(num2 != 0, "Can't divide by 0");
         return num1 % num2;
     }
